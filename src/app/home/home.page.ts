076354fa8918b4d75892f2721403289071b7b789
Felicia Ebikon <feliciaebikon@gmail.com>
2020-11-11 14:51:26 +0100

    deleted admin form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, RendererFactory2, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -13,68 +14,41 @@ import { auth } from 'firebase/app';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  renderer: Renderer2;
   adminForm: FormGroup;
   admin: any;
   currentUser: any;
   user: any;
-  isAdmin: boolean = false;
   
-
-  constructor(private fb: FormBuilder, 
+  constructor(
+    private fb: FormBuilder, 
+    private rendererFactory: RendererFactory2, 
+    @Inject(DOCUMENT) private document: Document,
     private aff: AngularFireFunctions,
     private db: AngularFirestore, 
     private auth: AuthService,
     public afAuth: AngularFireAuth,
     private blogService: BlogService) { 
+      this.renderer = rendererFactory.createRenderer(null, null);
       this.afAuth.authState.subscribe(user => {
         if (user) {
           
           this.currentUser = this.afAuth.auth.currentUser; 
-          console.log(this.currentUser);
 
-          // this.currentUser.getIdTokenResult().then(idTokenResult => {
-          //   if (idTokenResult.claims.admin) {
-          //     this.isAdmin = true;
-          //   }
-          // })
         }
       });
     }
 
-  ngOnInit() {
-    this.adminForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
-    });
-    this.afAuth.authState.subscribe((user) => {
-      user.getIdTokenResult().then(value => {
-        if (value.claims.admin) {
-          console.log('User is an admin')
-          this.isAdmin = true;
-        }
-      })
-    })
-   
+  ngOnInit() {   
   }
 
-    
-    
-  async signOut() {
-    this.afAuth.auth.signOut().then(() => {
-      console.log('successful logout');
-      this.currentUser = null;
-      this.currentUser.admin = null;
-      // future modal to confirm
-    });
+  enableLight() {
+    this.renderer.removeClass(this.document.body, 'dark-theme');
   }
 
-  async makeAdmin() {
-    const adminEmail = this.adminForm.value.email;
-    const addAdminRole = this.aff.httpsCallable('addAdminRole');
-    addAdminRole({ email: adminEmail }).subscribe(result => {
-     console.log(result);
-   });
- }
-
-
+  enableDark() {
+    this.renderer.addClass(this.document.body, 'dark-theme');
+  } 
 
 }
